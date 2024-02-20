94ff8cb084f7d0614c49f9edc606e4dca33b0b40
yuvwwa <yuvwwa@gmail.com>
2024-02-20 16:15:54 +0800

    исправила

--- numpy_task/main.py
+++ numpy_task/main.py
@@ -43,12 +43,12 @@ kl = np.power(k, l)
 assert np.all(kl == np.array([1, 4, 27, 64, 625]))
 
 m = np.array([2, 2, 2, 3, 3, 3])
-mc = np.sum(m)/30
+mc = m.std()
 
 assert mc == 0.5
 
 n = np.array([1, 2, 3, 4, 5, 6])
-nc = np.sum(n)/6
+nc = n.mean()
 
 assert nc == 3.5
 
@@ -63,7 +63,7 @@ pc = p[::-1]
 assert np.all(pc == np.array([4, 3, 2, 1]))
 
 r = np.array([3, 3, 5, 5])
-rc = np.array([3, 3, 5, 5])
+rc = r.copy()
 rc[1:3] = -1
 
 assert np.all(r[1:3] == np.array([3, 5])) and np.all(rc[1:3] == np.array([-1, -1]))
